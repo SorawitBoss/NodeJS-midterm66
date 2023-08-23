dd39c556e1d0151cefdbf030d3113ee5a8e7d5e6
SorawitBoss <sorawitph63@nu.ac.th>
2023-08-23 13:52:19 +0700

    secon

--- server/src/controllers/que1.js
+++ server/src/controllers/que1.js
@@ -1,41 +1,21 @@
-module.exports ={
-
-    add1 (req,res){
-        input = parseInt(JSON.stringify(req.body['input']))
-        console.log('input = '+ input)
-        output = input + 1
-        res.send(output.toString())
-    },
-
-    multiply (req,res){
-        X = parseInt(JSON.stringify(req.body['X']))
-        console.log('X = '+ X)
-        output = X * X
-        res.send(output.toString())
-    },
-
-    LCM (req,res){
-        X = parseInt(JSON.stringify(req.body['X']))
-        Y = parseInt(JSON.stringify(req.body['Y']))
-        console.log('X = '+ X)
-        console.log('Y = '+ Y)
-
-        for(i=X+Y;i>0;i--)
-        {
-            if(Y%i==0 && X%i==0){
-                gcd=i;
-                break;
-            }
-        }
-        console.log('GCD = '+ gcd)
-        mid=(X*Y)
-        if(mid<0){
-            mid=mid*-1
-        }
-        lcm = mid/gcd
-        res.send(lcm.toString())
-    }
-
-    
-
-}
+class que1 {
+    multiply(number) {
+        return number*number
+      }
+      plus(number) {
+        return number+number
+      }
+      deletedelete(number) {
+        return number--
+      }
+      plusplus(number) {
+        return number++
+      }
+      per(number) {
+        return number ,x=10; x--; //x=9
+      }
+     
+  }
+  
+  
+  module.exports = new que1() ;
