f0199c279f9afeff3360c8009348d250d98d398b
SorawitBoss <sorawitph63@nu.ac.th>
2023-08-23 13:24:33 +0700

    secon

--- server/src/controllers/que1.js
+++ server/src/controllers/que1.js
@@ -1,12 +1,41 @@
-export function activate(context) {
-    let api = {
-      sum(a, b) {
-        return a + b;
-      },
-      mul(a, b) {
-        return a * b;
-      }
-    };
-    // 'export' public api-surface
-    return api;
-  }
+module.exports ={
+
+    add1 (req,res){
+        input = parseInt(JSON.stringify(req.body['input']))
+        console.log('input = '+ input)
+        output = input + 1
+        res.send(output.toString())
+    },
+
+    multiply (req,res){
+        X = parseInt(JSON.stringify(req.body['X']))
+        console.log('X = '+ X)
+        output = X * X
+        res.send(output.toString())
+    },
+
+    LCM (req,res){
+        X = parseInt(JSON.stringify(req.body['X']))
+        Y = parseInt(JSON.stringify(req.body['Y']))
+        console.log('X = '+ X)
+        console.log('Y = '+ Y)
+
+        for(i=X+Y;i>0;i--)
+        {
+            if(Y%i==0 && X%i==0){
+                gcd=i;
+                break;
+            }
+        }
+        console.log('GCD = '+ gcd)
+        mid=(X*Y)
+        if(mid<0){
+            mid=mid*-1
+        }
+        lcm = mid/gcd
+        res.send(lcm.toString())
+    }
+
+    
+
+}
--- server/src/controllers/que2.js
+++ server/src/controllers/que2.js
@@ -1,6 +1,6 @@
 module.exports ={
 
-    que2 (req,res){
+    add2 (req,res){
         input = parseInt(JSON.stringify(req.body['input']))
         console.log('input = '+ input)
         output = input + 1
